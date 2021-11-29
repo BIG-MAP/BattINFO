419d3aa6c25b57963127947601ff95c9a0d540fa
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2021-11-29 16:02:48 +0100

    Change domain name for emmo & electrochemistry
    No need to update the catalog-v001.xml file before adding it to the
    gh-pages folders now, so the Python script has been streamlined
    accordingly.

--- .github/utils/publish_ontologies.py
+++ .github/utils/publish_ontologies.py
@@ -62,10 +62,7 @@ def main() -> None:
 
     local_ontologies = list(ROOT_DIR.glob("*.ttl"))
 
-    catalog_file_locations: List[Path] = []
-
     catalog_file = sorted(ROOT_DIR.glob("catalog-*.xml"), reverse=True)[0]
-    catalog_file_contents = catalog_file.read_text(encoding="utf8").splitlines()
 
     print("Publishing ontologies:")
     for ontology_file in local_ontologies:
@@ -93,25 +90,10 @@ def main() -> None:
             src=ontology_file,
             dst=absolute_publish_dir / relative_destination_dir / ontology_file.name,
         )
-
-        if not version_iri.startswith(PUBLISH_URL):
-            # Serve ontologies outside the BIG-MAP domain as part of these ontologies.
-            # Change the catalog URI, but not the name, i.e., download from the BIG-MAP
-            # domain.
-            catalog_file_contents = [
-                re.sub(
-                    fr"uri=('|\"){ontology_file.name}('|\")",
-                    f"uri=\"{PUBLISH_URL}/{relative_destination_dir}/{ontology_file.name}\"",
-                    line.rstrip(None),
-                ) for line in catalog_file_contents
-            ]
-
-        catalog_file_locations.append(absolute_publish_dir / relative_destination_dir)
-    
-    print("Add catalog-v001.xml files:")
-    for location in catalog_file_locations:
-        print(f"  * {location.relative_to(absolute_publish_dir)}")
-        (location / catalog_file.name).write_text("\n".join(catalog_file_contents) + "\n", encoding="utf8")
+        shutil.copyfile(
+            src=catalog_file,
+            dst=absolute_publish_dir / relative_destination_dir / catalog_file.name,
+        )
 
 
 if __name__ == "__main__":
