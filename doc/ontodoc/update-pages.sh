4cb9b4a96dcbd0fdf78eb973406b0330f8b2b0d1
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-03-02 17:22:14 +0100

    Update CI/CD for docs and update-pages.sh
    Update according to BIG-MAP/LabNotebookAppOntology.

--- doc/ontodoc/update-pages.sh
+++ doc/ontodoc/update-pages.sh
@@ -4,36 +4,43 @@ set -ex
 
 # Directories
 rootdir=$(git rev-parse --show-toplevel)
-ontodocdir=${rootdir}/${ONTODOC_DIR}
-tmpdir=${ontodocdir}/${TMP_DIR}
-pagesdir=${tmpdir}/${PAGES_DIR}
+ontodocdir=${rootdir}/${ONTODOC_DIR:-doc/ontodoc}
+tmpdir=${ontodocdir}/${TMP_DIR:-tmp}
+
+if [ -n "${GITHUB_WORKSPACE}" ]; then
+    # Running as a GH Action
+    pagesdir=${GITHUB_WORKSPACE}/../${PAGES_DIR}
+else
+    # Running locally
+    pagesdir=${tmpdir}/${PAGES_DIR:-gh-pages}
+fi
 
 # Generate documentation
-# ${ontodocdir}/mkdoc.sh
+if [ "$1" != "ALREADY_BUILT" ]; then
+    ${ontodocdir}/mkdoc.sh
+fi
 
 if [ "$1" = "TEST" ]; then
     echo "Not publishing - just testing (for CI)."
     exit
 fi
 
-# Checkout gh-pages
-if ! [ -d ${pagesdir} ]; then
-    git clone --branch=gh-pages --single-branch \
-        git@github.com:BIG-MAP/BattINFO.git ${pagesdir}
-    git config pull.rebase false
-fi
-
-# Update local copy of gh-pages
-cd ${pagesdir}
-git pull origin gh-pages
-
 # Copy documentation to gh-pages
 # FIXME - generate separate index.html with links to versions
-# cp -u ${tmpdir}/battinfo.html index.html
-# cp -u ${tmpdir}/battinfo.pdf .
+mkdir -p ${pagesdir}
+cp -f -u ${tmpdir}/battinfo.html ${pagesdir}/index.html
+cp -f -u ${tmpdir}/battinfo.pdf ${pagesdir}/
 
-# Update gh-pages
-if git add index.html battinfo.pdf ${PUBLISH_ONTOLOGIES_DIR}; then
-    git commit -m "Update BattINFO documentation & ontologies"
-    git push origin gh-pages
+if [ -n "${GITHUB_WORKSPACE}" ]; then
+    # Checkout gh-pages
+    cd ${GITHUB_WORKSPACE}
+    git checkout -f gh-pages
+
+    # Update gh-pages
+    if git add index.html battinfo.pdf ${PUBLISH_ONTOLOGIES_DIR:-ontology}; then
+        git commit -m "Update BattINFO documentation & ontologies"
+        git push origin gh-pages
+    else
+        echo "No changes to commit."
+    fi
 fi
