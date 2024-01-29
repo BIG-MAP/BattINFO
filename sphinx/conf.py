05ce175420b038eeddd72cad5ce8caca4ce3da96
Simon Clark <simon.clark@sintef.no>
2024-01-29 16:24:20 +0100

    add sphinx-design

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -35,6 +35,7 @@ autodoc_member_order = 'bysource'
 extensions = ['sphinxcontrib.globalsubs',
               'sphinx.ext.intersphinx',
               'sphinx.ext.autosectionlabel',
+              'sphinx_design'
               ]
 
 autosectionlabel_prefix_document = True
